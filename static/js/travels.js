a366bdf9e64f7e26c43d5a3adedc22b0d579475f
Yzh <351268691@qq.com>
2022-01-07 00:05:54 +0800

    travels

---
+++ static/js/travels.js
@@ -0,0 +1,50 @@
+window.travels = [
+    {
+        date: '20210922',
+        place: '绍兴',
+        days: ['鲁迅故居\xa0\xa0\xa0沈园'],
+        value: 0
+    },
+    {
+        date: '20201004',
+        place: '呼和浩特',
+        days: ['金刚座谁宝塔\xa0\xa0\xa0大召无量寺'],
+        value: 0
+    },
+    {
+        date: '20201003',
+        place: '鄂尔多斯',
+        days: ['响沙湾'],
+        value: 0
+    },
+    {
+        date: '20201005',
+        place: '包头',
+        days: ['北方兵器城\xa0\xa0\xa0赛汗塔拉城中草原'],
+        value: 0
+    },
+    {
+        date: '20190914',
+        place: '南京',
+        days: ['南京航天航空大学\xa0\xa0\xa0秦淮河\xa0\xa0\xa0中国科举博物馆\xa0\xa0\xa0东南大学', '明故宫\xa0\xa0\xa0中山陵\xa0\xa0\xa0南京大学'],
+        value: 0
+    },
+    {
+        date: '20180628',
+        place: '杭州',
+        days: [],
+        value: 1
+    },
+    {
+        date: '20170426',
+        place: '西安',
+        days: ['华山'],
+        value: 0
+    },
+    {
+        date: '20140926',
+        place: '重庆',
+        days: [],
+        value: 1
+    }
+]
